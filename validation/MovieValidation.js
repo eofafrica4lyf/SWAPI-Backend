f9cb266c1c76b47523694fbb249a7599944f6be7
Emmanuel Aboderin <emmanuel.aboderin@thebulb.africa>
2021-01-15 02:43:36 +0100

    limit no of characters in comment to 500

--- validation/MovieValidation.js
+++ validation/MovieValidation.js
@@ -11,9 +11,10 @@ exports.postCommentValidation = (data) => {
             "number.greater": "ID must be greater than zero",
             "any.required": "ID is required"
         }),
-        comment: Joi.string().trim().required().messages({
+        comment: Joi.string().trim().max(500).required().messages({
             "string.base": "comment must be a string",
-            "any.required": "comment is required"
+            "any.required": "comment is required",
+            "string.max": "A comment should have a maximum of 500 characters"
         })
     })
 
