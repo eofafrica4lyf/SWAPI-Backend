3babf9a5f9a29a58cadcd234e74e5df8076fb641
Emmanuel Aboderin <emmanuel.aboderin@thebulb.africa>
2021-01-15 03:21:50 +0100

    get comment count for each movie in the list of movies.

--- services/Movie.js
+++ services/Movie.js
@@ -14,9 +14,20 @@ class Movie{
              * going ahead to run a (somewhat redundant) sort function 
              * caters for any future changes
              * */
-            movies.data.results = movies.data.results.sort((a, b) => 
+            movies.data.results = await Promise.all(movies.data.results.sort((a, b) => 
                 (new Date(a.release_date) - new Date(b.release_date)))
-                .map(({episode_id, opening_crawl, release_date}) => ({episode_id, opening_crawl, release_date}))
+                .map( async ({episode_id, opening_crawl}) => {
+                    let commentCount = await models.comment.count({
+                        where: {
+                            movieId: episode_id
+                        }
+                    })
+                    return {
+                        commentCount, 
+                        episode_id, 
+                        opening_crawl
+                    }
+                }))
             return movies.data;
         } catch (error) {
             throw new ErrorHandler("Some error occured", 500)
