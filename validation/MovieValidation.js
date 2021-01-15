d8571e64e737f0e00589a684786fa510d076c11e
Emmanuel Aboderin <emmanuel.aboderin@thebulb.africa>
2021-01-15 16:49:29 +0100

    implement endpoint to get list of characters sorted by name or height, order and gender

--- validation/MovieValidation.js
+++ validation/MovieValidation.js
@@ -36,3 +36,39 @@ exports.movieIdValidation = async (data) => {
         abortEarly: false
     });
 };
+
+exports.getMovieCharactersValidation = async (data) => {
+    data.movieId = !isNaN(data.movieId) ? parseInt(data.movieId) : "";
+    data.sort = !isEmpty(data.sort) ? data.sort : "";
+    data.order = !isEmpty(data.order) ? data.order : "";
+    data.gender = !isEmpty(data.gender) ? data.gender : "";
+
+    const getMovieCharactersSchema = Joi.object({
+        movieId: Joi.number().min(1).required().messages({
+            "number.base": "Movie id must be a number",
+            "number.min": "Invalid movie id",
+            "any.required": "Movie id is required"
+        }),
+        sort: Joi.string().trim().valid("name", "height").required().messages({
+            "string.base": "Sort type field must be string",
+            "string.empty": "Sort type field is required",
+            "any.required": "Sort type field is required",
+            "any.only": "Sort type field must either be name or height"
+        }),
+        order: Joi.string().trim().valid("ASC", "DESC").required().messages({
+            "string.base": "Order type field must be string",
+            "string.empty": "Order type field is required",
+            "any.required": "Order type field is required",
+            "any.only": "Order type field must either be ASC or DESC"
+        }),
+        gender: Joi.string().trim().valid("all", "male", "female", "hermaphrodite", "n/a").required().messages({
+            "string.base": "Gender type field must be string",
+            "string.empty": "Gender type field is required",
+            "any.required": "Gender type field is required",
+            "any.only": "Gender type field must either be all or male or female or hermaphrodite or n/a"
+        })
+    });
+    return await getMovieCharactersSchema.validateAsync(data, {
+        abortEarly: false
+    });
+};
